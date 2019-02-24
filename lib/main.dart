52a2ab53b859bde5e655249e3edfaf07729363e3
Nimor111 <georgi.bojinov@hotmail.com>
2019-02-25 00:11:36 +0200

    Restructure project

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:grocery_list/modules/home/components/Home.dart';
+
 void main() => runApp(GroceryApp());
 
 class GroceryApp extends StatelessWidget {
-  Widget _drawer() {
-    return SizedBox(
-      width: 200,
-      child: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            Container(
-              height: 80.0,
-              child: DrawerHeader(
-                child: Container(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Text(
-                    'Actions',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.deepOrange[800],
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _logoSection(String image, double width, double height) {
-    return Image.asset(image, width: width, height: height, fit: BoxFit.cover);
-  }
-
-  Widget _titleSection(String text, IconData icon, Color color) {
-    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Container(
-        padding: const EdgeInsets.all(32),
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 32),
-        ),
-      ),
-      Icon(icon, color: color),
-    ]);
-  }
-
-  Widget _homeButtonSection(String text, Color color, Function onPressed) {
-    return Container(
-      padding: const EdgeInsets.only(left: 32, right: 32, bottom: 8, top: 8),
-      child: RaisedButton(
-        color: color,
-        child: Container(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-        onPressed: onPressed,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -81,21 +12,7 @@ class GroceryApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: Scaffold(
-        drawer: _drawer(),
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Grocery list'),
-        ),
-        body: ListView(children: [
-          _logoSection('images/shopping-cart.png', 300, 200),
-          _titleSection(
-              'My grocer', Icons.shopping_cart, Colors.deepOrange[800]),
-          _homeButtonSection('Lists', Colors.deepOrange[800], () => {}),
-          _homeButtonSection('New list', Colors.deepOrange[800], () => {}),
-          _homeButtonSection('Products', Colors.deepOrange[800], () => {}),
-        ]),
-      ),
+      home: Home(),
     );
   }
 }
---
+++ lib/modules/home/components/Home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  Widget _drawer() {
+    return SizedBox(
+      width: 200,
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              height: 80.0,
+              child: DrawerHeader(
+                child: Container(
+                  padding: const EdgeInsets.all(0.0),
+                  child: Text(
+                    'Actions',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange[800],
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _logoSection(String image, double width, double height) {
+    return Image.asset(image, width: width, height: height, fit: BoxFit.cover);
+  }
+
+  Widget _titleSection(String text, IconData icon, Color color) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Container(
+        padding: const EdgeInsets.all(32),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 32),
+        ),
+      ),
+      Icon(icon, color: color),
+    ]);
+  }
+
+  Widget _homeButtonSection(String text, Color color, Function onPressed) {
+    return Container(
+      padding: const EdgeInsets.only(left: 32, right: 32, bottom: 8, top: 8),
+      child: RaisedButton(
+        color: color,
+        child: Container(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: _drawer(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Grocery list'),
+      ),
+      body: ListView(children: [
+        _logoSection('images/shopping-cart.png', 300, 200),
+        _titleSection('My grocer', Icons.shopping_cart, Colors.deepOrange[800]),
+        _homeButtonSection('Lists', Colors.deepOrange[800], () => {}),
+        _homeButtonSection('New list', Colors.deepOrange[800], () => {}),
+        _homeButtonSection('Products', Colors.deepOrange[800], () => {}),
+      ]),
+    );
+  }
+}
