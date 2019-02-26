7d8b3f6bbc74c91fd22645d9127384ca455b88e8
Nimor111 <georgi.bojinov@hotmail.com>
2019-02-27 00:46:02 +0200

    Make ProductState private

--- lib/modules/products/containers/ProductContainer.dart
+++ lib/modules/products/containers/ProductContainer.dart
@@ -4,10 +4,10 @@ import 'package:grocery_list/modules/products/components/ProductList.dart';
 
 class ProductContainer extends StatefulWidget {
   @override
-  ProductState createState() => new ProductState();
+  _ProductState createState() => new _ProductState();
 }
 
-class ProductState extends State<ProductContainer> {
+class _ProductState extends State<ProductContainer> {
   @override
   Widget build(BuildContext context) {
     return ProductList();
