94c05421d76747eebb0b0466cb6d121c37bb2970
Nimor111 <georgi.bojinov@hotmail.com>
2019-03-18 18:25:29 +0200

    Add floating action button, refactor GroceryListDetail

--- lib/modules/lists/components/GroceryListDetail.dart
+++ lib/modules/lists/components/GroceryListDetail.dart
@@ -6,30 +6,35 @@ import 'package:grocery_list/modules/core/components/GroceryDrawer.dart';
 import 'package:grocery_list/modules/core/components/GroceryRaisedButton.dart';
 
 class GroceryListDetail extends StatelessWidget {
-  final products = <Widget>[ProductItem(), ProductItem(), ProductItem()];
+  final products = <Widget>[
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem(),
+    ProductItem()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: GroceryBar(title: 'List detail'),
       drawer: GroceryDrawer(),
-      body: ListView(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      body: Column(
         children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(32),
-            child: GroceryRaisedButton(
-              color: Colors.deepOrange[800],
-              title: 'Add product',
-              textColor: Colors.white,
-              onPressed: () => {},
-            ),
-          ),
-          ListView.builder(
-            itemCount: products.length,
-            itemBuilder: (BuildContext context, int index) {
-              return products[index];
-            },
-            shrinkWrap: true,
+          Expanded(
+            child: ListView(children: products),
           ),
         ],
       ),
