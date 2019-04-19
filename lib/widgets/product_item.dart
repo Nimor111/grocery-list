7f7014b2aa394491630f87e9f8381e932b8243fd
Nimor111 <georgi.bojinov@hotmail.com>
2019-04-20 00:07:39 +0300

    Refactor ProductItem

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,7 +3,11 @@ import 'package:grocery_list/models/product.dart';
 import 'package:grocery_list/widgets/inherited/with_delete.dart';
 
 class ProductItem extends StatefulWidget {
-  ProductItem({this.product, this.listId, this.removeFromList, this.addToList});
+  ProductItem(
+      {@required this.product,
+      this.listId,
+      this.removeFromList,
+      this.addToList});
 
   final Product product;
   final Function removeFromList;
@@ -30,9 +34,46 @@ class _ProductItemState extends State<ProductItem> {
     );
   }
 
+  Widget _leadingSection() {
+    return widget.listId != null
+        ? IconButton(
+            icon: Icon(Icons.done, color: Colors.green),
+            onPressed: () {
+              this.setState(() {
+                enabled = !enabled;
+              });
+            })
+        : Icon(Icons.apps);
+  }
+
+  Function _longPressAction() {
+    return widget.addToList != null
+        ? () {
+            widget.addToList(widget.product, widget.listId);
+            Scaffold.of(context).showSnackBar(SnackBar(
+              content: Text("Product added!"),
+            ));
+          }
+        : () {};
+  }
+
+  Function _dismissedAction() {
+    final deleteWidget = WithDelete.of(context);
+    return deleteWidget != null
+        ? () {
+            deleteWidget.delete(widget.product.documentID);
+
+            _showSnackbar(context, "Product deleted");
+          }
+        : () {
+            widget.removeFromList(widget.listId, widget.product);
+
+            _showSnackbar(context, "Product removed from list");
+          };
+  }
+
   @override
   Widget build(BuildContext context) {
-    final deleteWidget = WithDelete.of(context);
     return Container(
       padding: const EdgeInsets.all(8),
       child: Card(
@@ -41,39 +82,19 @@ class _ProductItemState extends State<ProductItem> {
             Dismissible(
               key: Key(widget.product.documentID),
               onDismissed: (_direction) {
-                if (deleteWidget != null) {
-                  deleteWidget.delete(widget.product.documentID);
-
-                  _showSnackbar(context, "Product deleted");
-                } else {
-                  widget.removeFromList(widget.listId, widget.product);
-
-                  _showSnackbar(context, "Product removed from list");
-                }
+                final Function action = _dismissedAction();
+                action();
               },
               child: ListTile(
-                leading: IconButton(
-                  icon: Icon(Icons.done, color: Colors.green),
-                  onPressed: () {
-                    this.setState(() {
-                      enabled = !enabled;
-                    });
-                  },
-                ),
+                leading: _leadingSection(),
                 enabled: enabled,
-                title: Text(widget.product != null
-                    ? widget.product.name
-                    : 'Product name'),
+                title: Text(widget.product.name),
                 subtitle: Text('Amount'),
                 onTap: () => _pushProductDetail(context),
-                onLongPress: widget.addToList != null
-                    ? () {
-                        widget.addToList(widget.product, widget.listId);
-                        Scaffold.of(context).showSnackBar(SnackBar(
-                          content: Text("Product added!"),
-                        ));
-                      }
-                    : null,
+                onLongPress: () {
+                  final Function action = _longPressAction();
+                  action();
+                },
               ),
             ),
           ],
